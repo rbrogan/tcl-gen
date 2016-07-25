5b179ece1d200f5fc3493792c3fa5f6500bfff19
Robert Brogan <rbrogan3@yahoo.com>
2016-07-25 19:54:57 -0400

    1.16.0

--- test/all.tcl
+++ test/all.tcl
@@ -2,6 +2,6 @@ package require tcltest
 ::tcltest::configure -testdir \
 	[file dirname [file normalize [info script]]]
 eval ::tcltest::configure $argv
-#eval ::tcltest::configure -skip {Ftp* *Email*}
+eval ::tcltest::configure -skip Ftp*
 ::tcltest::runAllTests
 
